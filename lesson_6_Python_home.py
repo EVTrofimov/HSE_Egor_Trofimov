302512dbdaa7d7644dd696ca61718b1713169625
EVTrofimov <150274496+EVTrofimov@users.noreply.github.com>
2024-05-12 00:06:55 +0300

    Update lesson_6_Python_home.py

--- lesson_6_Python_home.py
+++ lesson_6_Python_home.py
@@ -1,32 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # домашнее задание 11
-# 
-# Напишите класс LegalAPI для взаимодействия с API, размещённым по адресу
-# https://legal-api.sirotinsky.com/.
-# 
-# Класс должен содержать в себе функционал для работы со всеми методами,
-# указанными в документации к API, которые размещены по адресу
-# https://legal-api.sirotinsky.com/docs
-# 
-# Метод инициализации экземпляра класса должен принимать в качестве аргумента токен для авторизации.
-# 
-# Методы для получения данных из ЕФРСБ должны быть публичными и содержать
-# doc string
-# 
-# Данные для доступа:
-# Token: 4123saedfasedfsadf4324234f223ddf23
-
-# In[1]:
+# домашнее задание 11
 
 
 # 0 --- импортируем библиотеки
 
 import requests
-
-
-# In[2]:
 
 
 # 1 --- создаем класс
@@ -91,73 +68,34 @@ class LegalAPI:
         return requests.get(f'https://legal-api.sirotinsky.com/{self.token}/efrsb/organisation/{inn}').json()
 
 
-# In[3]:
-
-
-# 2 --- создаем экземпляр
+# 2 --- создаем экземпляр и выводим docstring
 
 a = LegalAPI(token = '4123saedfasedfsadf4324234f223ddf23')
 get_ipython().run_line_magic('pinfo', 'a')
 
 
-# In[4]:
+# 3 --- используем методы и выводим docstring
 
-
-get_ipython().run_line_magic('pinfo', 'a.efrsb_publisher_messages')
 print('\nefrsb_publisher_messages\n\n', a.efrsb_publisher_messages(inn = '780533976702'))
+get_ipython().run_line_magic('pinfo', 'a.efrsb_publisher_messages')
 
-
-# In[5]:
-
-
-get_ipython().run_line_magic('pinfo', 'a.efrsb_debtor_messages')
 print('\nefrsb_debtor_messages\n\n', a.efrsb_debtor_messages(inn = '7810368236'))
+get_ipython().run_line_magic('pinfo', 'a.efrsb_debtor_messages')
 
-
-# In[6]:
-
-
-get_ipython().run_line_magic('pinfo', 'a.efrsb_manager_all')
 print('\nefrsb_manager_all\n\n', a.efrsb_manager_all())
+get_ipython().run_line_magic('pinfo', 'a.efrsb_manager_all')
 
-
-# In[7]:
-
-
-get_ipython().run_line_magic('pinfo', 'a.efrsb_manager')
 print('\nefrsb_manager\n\n', a.efrsb_manager(inn = '191001529230'))
+get_ipython().run_line_magic('pinfo', 'a.efrsb_manager')
 
-
-# In[8]:
-
-
-get_ipython().run_line_magic('pinfo', 'a.efrsb_trader_all')
 print('\nefrsb_trader_all\n\n', a.efrsb_trader_all())
+get_ipython().run_line_magic('pinfo', 'a.efrsb_trader_all')
 
-
-# In[9]:
-
-
-get_ipython().run_line_magic('pinfo', 'a.efrsb_trader')
 print('\nefrsb_trader\n\n', a.efrsb_trader(inn = '2312171566'))
+get_ipython().run_line_magic('pinfo', 'a.efrsb_trader')
 
-
-# In[10]:
-
-
-get_ipython().run_line_magic('pinfo', 'a.efrsb_person')
 print('\nefrsb_person\n\n', a.efrsb_person(inn = '667801453240'))
+get_ipython().run_line_magic('pinfo', 'a.efrsb_person')
 
-
-# In[11]:
-
-
-get_ipython().run_line_magic('pinfo', 'a.efrsb_organisation')
 print('\nefrsb_organisation\n\n', a.efrsb_organisation(inn = '7206038280'))
-
-
-# In[ ]:
-
-
-
-
+get_ipython().run_line_magic('pinfo', 'a.efrsb_organisation')
